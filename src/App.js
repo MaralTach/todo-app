ad7af5bedb0b494fda98c99b6511084684e0f4b1
MaralTach <yoloten.maral@gmail.com>
2024-04-08 16:11:53 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import Header from './components/Header';
 import TodoList from './components/TodoList';
 import {useState} from 'react'
 
+const todoList = [
+  {
+    id:1,
+    task:"Todo Something",
+    completed:false,
+  },
+  {
+    id:new Date().getTime(),
+    task:"Ract todo List",
+    completed:false,
+  },
+ 
+]
 function App() {
-  const [todo, setTodo] = useState ([])
+  const [todo, setTodo] = useState (todoList)
   return (
     <div className="container">
      <Header setTodo={setTodo} todo={todo} />
-     <TodoList todo={todo}/>
+     <TodoList todo={todo}  setTodo={setTodo}/>
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import {ListGroup} from 'react-bootstrap';
 
-const TodoList = ({todos}) => {
+const TodoList = ({todo, setTodo}) => {
+
+  const deleteTodo = (id)=>{
+    const newTodos = todo.filter(todo => todo.id !== id)
+      setTodo(newTodos)
+  }
+
+
   return (
     <div>
         <h2 className='text-center '>Todos</h2>
         <ListGroup className='w-50 mx-auto d-flex'>
-        <ListGroup.Item variant="secondary">Secondary</ListGroup.Item>
-{/* {
-    todos.map((todo) =>)
-} */}
+     
+
+
+        {
+            todo.map((todo)=> 
+            <ListGroup.Item variant="success" className='m-2 text-capitalize rounded-4 d-flex justify-content-between'> <span>{todo.task}</span>  
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+            onClick={()=>deleteTodo(todo.id)}
+            role='button'
+            fill="red" classNames="bi bi-trash-fill" viewBox="0 0 16 16">
+            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+            </svg>
+            </ListGroup.Item>)
+        }
+
 
         </ListGroup>
     </div>
